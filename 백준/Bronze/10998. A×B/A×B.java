0d52471d645a782b1ee9774a8fa5c04544934bc7
Jiwon <64239648+jiwon709@users.noreply.github.com>
2023-01-07 22:52:05 +0900

    [Bronze V] Title: A×B, Time: 216 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        if (a > 0 && b < 10) {
+            System.out.println(a * b);
+        }
+    }
+}
